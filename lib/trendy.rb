60585ed422d57d551725b48818e2c84298568dc4
Paul Fedory <paul.fedory@gmail.com>
2010-01-26 09:44:58 +0000

    refactoring

--- lib/trendy.rb
+++ lib/trendy.rb
@@ -1,38 +1,43 @@
 require 'open-uri'
 require 'rubygems'
-require 'json'
+require 'json/pure'
 
 # Trendy
 module Trendy
-
+  
+  # Trends is an array which contains Trend objects
+  # use these Trend objects to access information about a particular trend
   class Trends < Array
+    attr_reader :url, :datetime
     
-    attr_accessor :url, :datetime
     def initialize
     
       @url = 'http://search.twitter.com/trends.json'
-
-      buffer = open(url, "UserAgent" => "Ruby-Wget").read
-
-      # convert JSON data into a hash
-      result = JSON.parse(buffer)
+      result = read_twitter
 
       trends = result['trends']
       
       @datetime = result['as_of']
-      puts @datetime.class.to_s
       
       trends.each do |subject|
         trend = Trend.new
         trend.name = subject['name']
         trend.url = subject['url']
         self.push(trend)
-        #puts subject['name'] + ' ' + subject['url']
       end
-
     end
+    
+    private
+    def read_twitter
+      buffer = open(@url, "UserAgent" => "Ruby-Wget").read
+
+      # convert JSON data into a hash
+      result = JSON.parse(buffer)
+    end
+    
   end
   
+  # Trend contains the name, url (for current trends) or query (for older trends) of a single trending topic
   class Trend
     attr_accessor :name, :url, :query
   end
